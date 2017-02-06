88e62a337c2b6aa893d79d63cf7823e21a38d7a7
Ibrahim Salim Meman <P15224747@LEC-ADMIN.dmu.ac.uk>
2017-02-06 14:24:36 +0000

    Min and Max values coded

--- calculator.c
+++ calculator.c
@@ -2,9 +2,28 @@
 
 int main()
 {
+	int arr[6],min,max;
+	printf("enter 5 integers:");
+	int z;
+	for (z=0;z < 6;z++){
+		
+		scanf(%i,&arr[z]);
+	}
 	
+	min=max=a[0];
+	int size=sizeof(a)/sizeof(int);
+	int o,p;
+	for(o=0;o<size;o++){
+		for(p=o+1;p<size;j++){
+			if(min > a[p]){	
+			    min =a[p];}
+			if (max <a[p]){
+			    max = a[p]}
+			}
+		}
+	printf("Max=%d and Min=%d",max,min);
 	
-	char operator;
+    char operator;
     double i,j;
 
     printf("Enter an operator (+, -, *,): ");
