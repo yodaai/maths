a3945d18c6718972491cc8223f24bec443a70ae0
Ibrahim Salim Meman <P15224747@LEC-ADMIN.dmu.ac.uk>
2017-02-06 13:42:14 +0000

    calculator functions

---
+++ calculator.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+	
+	
+	char operator;
+    double i,j;
+
+    printf("Enter an operator (+, -, *,): ");
+    scanf("%c", &operator);
+
+    printf("Enter two operands: ");
+    scanf("%lf %lf",&i, &j);
+
+    switch(operator)
+    {
+        case '+':
+            printf("%.1lf + %.1lf = %.1lf",i, j, i + j);
+            break;
+		case '-':
+            printf("%.1lf - %.1lf = %.1lf",i, j, i - j);
+            break;
+
+        case '*':
+            printf("%.1lf * %.1lf = %.1lf",i, j, i * j);
+            break;
+			    case '/':
+            printf("%.1lf / %.1lf = %.1lf",i, j, i / i);
+            break;
+
+        // operator doesn't match any case constant (+, -, *, /)
+        default:
+            printf("Error! operator is not correct");
+    }
+    
+    return 0;
+}
+
